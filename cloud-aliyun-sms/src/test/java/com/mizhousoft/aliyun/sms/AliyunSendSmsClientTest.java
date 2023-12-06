431711affaa5058b9bbda67874ebb1e1736bcc69
jacklee <lsp_lishuiping@qq.com>
2023-12-06 09:25:05 +0800

    解决短信发送bug

--- cloud-aliyun-sms/src/test/java/com/mizhousoft/aliyun/sms/AliyunSendSmsClientTest.java
+++ cloud-aliyun-sms/src/test/java/com/mizhousoft/aliyun/sms/AliyunSendSmsClientTest.java
@@ -18,26 +18,28 @@ import com.mizhousoft.cloudsdk.sms.SmsSendException;
  */
 public class AliyunSendSmsClientTest
 {
-	private SendSmsClient sendSmsClient;
+	private static SendSmsClient sendSmsClient;
 
 	@BeforeAll
 	public static void init() throws CloudSDKException
 	{
 		SmsProfile profile = new SmsProfile();
-		profile.setAccessKeyId(null);
-		profile.setAccessKeySecret(null);
-		profile.setEndpoint(null);
+		profile.setAccessKeyId("");
+		profile.setAccessKeySecret("");
+		profile.setEndpoint("dysmsapi.aliyuncs.com");
 
 		AliyunSendSmsClient smsClient = new AliyunSendSmsClient();
 		smsClient.init(profile);
+
+		sendSmsClient = smsClient;
 	}
 
 	@Test
 	public void send()
 	{
 		String phoneNumber = "";
-		Map<String, String> paramMap = null;
-		CloudSmsTemplate smsTemplate = null;
+		Map<String, String> paramMap = Map.of("verificationCode", "123456", "validTime", "10");
+		CloudSmsTemplate smsTemplate = new CloudSmsTemplate(null, "", "");
 
 		try
 		{
