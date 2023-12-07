fa8c20ccf846d98d44ddfd139e9ce6a30e0959bc
jacklee <lsp_lishuiping@qq.com>
2023-12-07 08:19:12 +0800

    新增cloud-tencent-tms工程

---
+++ cloud-tencent-tms/src/main/java/com/mizhousoft/tencent/RegionEnum.java
@@ -0,0 +1,61 @@
+package com.mizhousoft.tencent;
+
+/**
+ * 区域枚举
+ *
+ * @version
+ */
+public enum RegionEnum
+{
+    // 广州
+	GUANGZHOU("ap-guangzhou"),
+    // 广州2
+	GUANGZHOU2("ap-guangzhou-2"),
+    // 上海
+	SHANGHAI("ap-shanghai");
+
+	/**
+	 * 构造函数
+	 *
+	 * @param val
+	 */
+	private RegionEnum(String val)
+	{
+		this.value = val;
+	}
+
+	/**
+	 * 值
+	 */
+	private final String value;
+
+	/**
+	 * 获取value
+	 * 
+	 * @return
+	 */
+	public String getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * 获取状态
+	 * 
+	 * @param status
+	 * @return
+	 */
+	public static RegionEnum get(String value)
+	{
+		RegionEnum[] values = RegionEnum.values();
+		for (RegionEnum val : values)
+		{
+			if (val.getValue().equals(value))
+			{
+				return val;
+			}
+		}
+
+		return null;
+	}
+}
